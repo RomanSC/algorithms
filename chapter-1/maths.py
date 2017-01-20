a07dba0c60e392007c6c16d062a51e84c5a2432a
RomanSC <r.s.col988@gmail.com>
2017-01-20 16:14:24 -0500

    Explaining def get_common_denomitor function, and solving sigma 100 n = 1, where the function will be 1 over i squared.

--- chapter-1/maths.py
+++ chapter-1/maths.py
@@ -8,8 +8,18 @@
 import doctest
 
 class Fraction:
-    def __init__(self, title, numerator, denominator):
-        self.title = title
+    """ The Fractions class as
+
+        Fractions - Doctest:
+        >>> (1 / 4) + (1 / 2)
+        0.75
+        >>> 3 / 4
+        0.75
+        >>> print(Fraction(1, 4))
+        1/4
+    """
+
+    def __init__(self, numerator, denominator):
         self.numerator = numerator
         self.denominator = denominator
 
@@ -21,21 +31,55 @@ class Fraction:
         new_numerator = (self.numerator * plus.denominator) + (self.denominator * plus.numerator)
         new_denominator = (self.denominator * plus.denominator)
 
-        common_denominator = gcd(new_numerator, new_denominator)
+        common_denominator = self.get_common_denominator(new_numerator, new_denominator)
 
-        return Fraction('Fraction', new_numerator // common_denominator, new_denominator // common_denominator)
+        return Fraction(new_numerator // common_denominator, new_denominator // common_denominator)
 
     def __eq__(self, compare):
-        pass
         first_number = (self.numerator * compare.denominator)
         second_number = (compare.numerator * self.denominator)
 
         return first_number == second_number
 
-class Sum:
-    def __init__(self, title, upper_bound, lower_bound):
-        self.title = title
+    def get_common_denominator(self, fract_one, fract_two):
+        """ Like the counting function but using the
+            Fractions class in this program.
 
+            "Is this one lower? OKAY this one is now
+            that one. Is this one lower? OKAY this
+            one is now that one. Is this one lower!?
+            No. :( Okay, I'll stop..."
+
+            Use floor division to store the fractions
+            in lowest terms until they are closest to
+            zero or zero but no less.
+
+            i.e. Common denominators are found, then
+            the two fractions are divided.
+
+            Doctest:
+            >>> (1 / 3) % (1 / 6)
+            0.0
+            >>> (1 / 32) % (7 / 42)
+            0.03125
+        """
+        while fract_one % fract_two != 0:
+            old_fract_one = fract_one
+            old_fract_two = fract_two
+
+            fract_one = old_fract_two
+            fract_two = old_fract_one % old_fract_two
+
+        return fract_two
+
+
+class Sum:
+    """ Summation - Doctest:
+        >>> print(Sum(400, 1))
+        400
+    """
+
+    def __init__(self, upper_bound, lower_bound):
         for self.lower_bound in range(upper_bound + 1):
             if self.lower_bound <= upper_bound:
                 self.lower_bound =+ self.lower_bound
@@ -48,32 +92,11 @@ class Sum:
 #        #print(self.lower_bound)
 #        return self.lower_bound
 
-def gcd(m, n):
-    while m % n != 0:
-        oldm = m
-        oldn = n
-
-        m = oldn
-        n = oldm % oldn
-
-    return n
-
-    #print(gcd(20,32))
-
 def main():
-    """ Fractions - Doctest:
-    >>> (1 / 4) + (1 / 2)
-    0.75
-    >>> 3 / 4
-    0.75
-    >>> print(Fraction('Fraction', 1, 4))
-    1/4
-    """
-
-    fract1 = Fraction('Fraction', 1, 4)
-    fract2 = Fraction('Fraction', 1, 2)
-    fract3 = Fraction('Fraction', 1, 2)
-    fract4 = Fraction('Fraction', 1, 2)
+    fract1 = Fraction(1, 4)
+    fract2 = Fraction(1, 2)
+    fract3 = Fraction(1, 2)
+    fract4 = Fraction(1, 2)
 
 
     print('Fraction: {}'.format(fract1), '+', fract2)
@@ -82,22 +105,45 @@ def main():
     print('{} is equal to {}?: '.format(fract3, fract4))
     print(fract3 == fract4, '\n')
 
-    """ Summation - Doctest:
-    >>> print(Sum('Sigma', 400, 1))
-    400
-    """
+    #print(Sum('Sigma', 100, Fraction(1, (1 / 1 ** 2))))
 
-    print(Sum('Sigma', 100, Fraction('Fraction', 1, (1 / 1 ** 2))))
+    #print(Sum('Sigma', 100, Fraction(1, (1 / n ** 2) for n in range(100))))
 
-    #print(Sum('Sigma', 100, Fraction('Fraction', 1, (1 / n ** 2) for n in range(100))))
-    ub = 100
-    lb = 1
+    #for i in range(ub):
+    #    #print(i)
+    #    i = i ** 2
+    #    n = Fraction(lb, i)
+    #    total = Sum('Sigma', ub, n)
+    #    #total = Sum('Sigma', ub, Fraction(lb, (1 / i ** 2)))
+    #    #print(total)
 
-    for i in range(ub):
-        total = Sum('Sigma', ub, Fraction('Fraction', lb, (1 / i ** 2)))
-        #print(total)
+    iterations = []
 
-    print(total)
+    for i in range(100 + 1):
+        if i >= 1:
+            n = (i ** 2)
+            iteration = (1 / n)
+            #iteration = Fraction(1, n)
+
+        try:
+            #print(iteration)
+            iterations.append(iteration)
+        except:
+            if i == 0:
+                iterations.append(i)
+            # Except pass here, so that
+            # if "function" is equal to
+            # zero, UnboundLocalError
+            # is not thrown and the
+            # can proceed
+            pass
+
+    #print(len(iterations))
+    print(sum(iterations))
+
+    #Sum('Sigma', 100, function)
+
+    #print(total)
 
 if __name__ == '__main__':
     doctest.testmod()
