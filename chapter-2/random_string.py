6c343a0a077da70e266ef50d75c06a6ba69d4c91
RomanSC <r.s.col988@gmail.com>
2017-02-01 13:26:28 -0500

    Wrote random strings function

--- chapter-2/random_string.py
+++ chapter-2/random_string.py
@@ -1,3 +1,32 @@
 """ random_string.py | Wed, Feb 01, 2017 | Roman S. Collins
+
+    Demonstrating returning random strings in Python.
+
 """
 import random
+
+class randStr:
+    def rand_string(self, abc, n):
+        if abc == None:
+            abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                        'w', 'x', 'y', 'z', ' ']
+        abc_copy = list(abc[x] for x in range(len(abc)))
+        return ''.join(random.sample(abc_copy, n))
+
+def main():
+    """ Again only sanity checks, because output of the
+        function changes.
+
+        Doctest:
+        >>> len(randStr().rand_string(10))
+        10
+        >>> isinstance(randStr().rand_string(10), str)
+        True
+    """
+    # Fill main with something other than pass
+    while isinstance(randStr().rand_string(None, 10), str):
+        break
+
+if __name__=='__main__':
+    main()
