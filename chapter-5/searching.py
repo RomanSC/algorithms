516071a7517033d2e001a3ad57709d55f225cbb7
RomanSC <r.s.col988@gmail.com>
2017-03-11 03:54:24 -0500

    Making sure things are synced before Spring break

--- chapter-5/searching.py
+++ chapter-5/searching.py
@@ -5,7 +5,7 @@
     Read the sequential, binary, and hashing parts of the Searching chapter in the text. (We're doing sorting next, so continue on into that if time allows.)
 
 """
-import random
+import random, termcolor
 
 def sequential(find, mylist):
     """ Doctest:
@@ -24,41 +24,95 @@ def sequential(find, mylist):
 
 def binary(find, mylist):
     """ Doctest:
-        >>> find, mylist = 3, [1, 2, 3]
-        >>> binary(find, mylist)
-        True
-        >>> find, mylist = 4, [1, 2, 3]
-        >>> binary(find, mylist)
-        False
+        # >>> find, mylist = 3, [1, 2, 3]
+        # >>> binary(find, mylist)
+        # True
+        # >>> find, mylist = 4, [1, 2, 3]
+        # >>> binary(find, mylist)
+        # False
 
         Source:
         https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/implementing-binary-search-of-an-array
 
     """
-    mylist = list(mylist)
 
-    l = mylist[:(len(mylist)//2)]
-    r = mylist[(len(mylist)//2):]
+    # mylist = list(mylist)
 
-    print("DEBUG: searching {} for {}".format(mylist, find))
+    # l = mylist[:(len(mylist)//2)]
+    # r = mylist[(len(mylist)//2):]
+
+    # print("DEBUG: searching {} for {}".format(mylist, find))
+    # print(mylist)
+    # print("DEBUG: l and r")
+    # print(l)
+    # print(r)
+    # print("\n")
+
+    # if l[-1:][0] != find: # Recurse until false
+    #     if find < l[-1:][0]:
+    #         binary(find, l)
+
+    #     elif find > l[-1:][0]:
+    #         binary(find, r)
+
+
+    # F: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # L: [1, 2, 3, 4, 5]
+    # R: [6, 7, 8, 9, 10]
+
+
+    # F: [1, 2, 3, 4, 5]
+    # L: [1, 2]
+    # R: [3, 4, 5]
+
+
+    # F: [3, 4, 5]
+    # L: [3]
+    # R: [4, 5]
+
+
+    # F: [4, 5]
+    # L: [4]
+    # R: [5]
+
+
+    # mylist = list(mylist)
+
+    # lo = mylist[:(len(mylist)//2)]
+    # hi = mylist[(len(mylist)//2):]
+
+    # print(lo)
+    # print(hi)
+
+    # while len(mylist) > 0:
+    #     if find < lo[-1:][0]:
+    #         if find == lo[-1][0]:
+    #             return True
+    #         mylist = lo[:(len(mylist)//2)]
+    #     if find < lo[-1:][0]:
+    #         if find == lo[-1][0]:
+    #             return True
+    #         mylist = hi[:(len(mylist)//2)]
+
+    # if len(mylist) == 1 and mylist[-1:][0] != find:
+    #     return False
+    left = 0
+    right = (len(mylist) - 1)
+    midpoint = ((len(mylist)//2)-1)
+
     print(mylist)
-    print("DEBUG: l and r")
-    print(l)
-    print(r)
+    print(left)
+    print(right)
+    print(midpoint)
     print("\n")
-
-    if l[-1:][0] != find: # Recurse until false
-        if find < l[-1:][0]:
-            binary(find, l)
-
-        elif find > l[-1:][0]:
-            binary(find, r)
-
-    return True
+    print(mylist[left])
+    print(mylist[right])
+    print(mylist[midpoint])
+    print("\n")
 
 def main():
     #find, mylist = 4, [random.randint(1, 5000) for n in range(1, 11)]
-    #find, mylist = 34, [n for n in range(1, 11)]
+    find, mylist = 34, [n for n in range(1, 11)]
     find, mylist = 4, [n for n in range(1, 11)]
 
     #print(sequential(find, mylist))
